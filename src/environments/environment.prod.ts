d533b1f4831960522d84da5300cee087c9241438
Goldarmy <allanya2004@yahoo.com>
2020-09-12 19:06:57 -0400

    Updating npm packages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://cis-565.appspot.com/api"
-  //apiUrl: "https://morning-gorge-94271.herokuapp.com/api"
+  //apiUrl: "https://cis-565.appspot.com/api"
+  apiUrl: "https://morning-gorge-94271.herokuapp.com/api"
 };
