11d66026724d105bf4adccf8d66202b02d680687
Goldarmy <allanya2004@yahoo.com>
2020-09-13 12:01:36 -0400

    disabling iny and upadating angular material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,21 @@
 import { NgModule } from '@angular/core';
-import { MatTabsModule, MatSnackBarModule, MatListModule, MatSelectModule, MatDialogModule, MatDividerModule, MatButtonToggleModule, MatRadioModule, MatInputModule, MatIconModule, MatBadgeModule, MatToolbarModule, MatMenuModule, MatCardModule, MatButtonModule, MatProgressSpinnerModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 const MaterialComponents = [
   MatCardModule,
@@ -21,7 +37,7 @@ const MaterialComponents = [
 ]
 
 @NgModule({
-  imports: [MaterialComponents],
+  //imports: [MaterialComponents],
   exports: [MaterialComponents]
 })
 export class MaterialModule { }
