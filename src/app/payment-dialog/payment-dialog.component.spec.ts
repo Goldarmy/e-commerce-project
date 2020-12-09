3144f827117505c7eab84ee970f823c9ce8e2c26
Goldarmy <allanya2004@yahoo.com>
2020-12-09 11:20:09 -0500

    upgrade angular core & cli to version 10

--- src/app/payment-dialog/payment-dialog.component.spec.ts
+++ src/app/payment-dialog/payment-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PaymentDialogComponent } from './payment-dialog.component';
 
@@ -6,7 +6,7 @@ describe('PaymentDialogComponent', () => {
   let component: PaymentDialogComponent;
   let fixture: ComponentFixture<PaymentDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PaymentDialogComponent ]
     })
